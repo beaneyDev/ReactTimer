e8a641053b9481e7ce147058884fbe2732f60b1f
Matt Beaney <matt.beaney@pagesuite.co.uk>
2016-12-15 21:17:59 +0000

    Added countdown form

--- app/components/Clock.jsx
+++ app/components/Clock.jsx
@@ -23,6 +23,7 @@ var Clock = React.createClass({
   },
   render: function () {
     var {totalSeconds} = this.props;
+    console.log(totalSeconds);
     return (
       <div className="clock">
         <span className="clock-text">
--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,8 @@ module.exports = {
       Navigation: 'app/components/Navigation.jsx',
       Timer: 'app/components/Timer.jsx',
       CountDown: 'app/components/CountDown.jsx',
-      Clock: 'app/components/Clock.jsx'
+      Clock: 'app/components/Clock.jsx',
+      CountDownForm: 'app/components/CountDownForm.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
